e5dd73a3cad160f0b8f6522b24087d4440377120
JayceB17 <jayceb.ham17@yahoo.com>
2023-08-24 21:43:07 -0600

    fixing errors

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
-
+import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -63,7 +55,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+  
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -73,7 +65,7 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook(bookToSave, token);
-
+      
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
